151f876d269c0712cc0f248c28ffb1eb729e11cb
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-04-23 20:41:19 +0300

    1:m, m:m, relations

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,52 +1,39 @@
 -- Ключ - ознака (найчастіше штучна) яка відрізняє один запис у таблиці від іншого
 -- Первинний ключ (PRIMARY KEY) - використовується для того, щоб ключу дати обмеження унікальності (UNIQUE) і обмеження NOT NULL
+-- Група стовпців - стовпець (група стовпців) які могли статти первинним ключем, але ще не обрані як такі.
+-- Зовнішній ключ (foreing keys) - стовпець (група стовпців) які містять значення, які посилаються на ідентифікатори в інших таблицях 
 
-/*
-
-Створити таблицю books
-
-В книги є:
-1. Порядковий номер
-2. Автор книги
-3. Назва книги
-4. Рік випуску
-5. Видавництво
-6. Категорія
-7. Синопсиси 
-8. Кількість примірників
-9. Статус -- книга видана / не видана 
-
-ALTER: 
-
-1. Додати Lо таблиці обмеження, що кількість примірників (пункт 8) не може бути менше 0
-
-* 2. Додати до таблиці обмеження, що автор + назва книги не може бути пустим рядкои і має бути унікальним
-
-*/
-
-CREATE TABLE books(
+CREATE TABLE orders(
     id serial PRIMARY KEY,
-    author varchar(256),
-    name varchar(300),
-    year varchar(4),
-    publisher varchar(256),
-    category varchar(256),
-    synopsys text,
-    quantity int,
-    status boolean
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    customer_id int REFERENCES users(id)
 );
 
-ALTER TABLE books
-ADD CONSTRAINT "quaintity_more_zero" CHECK (quantity >= 0);
+DROP TABLE orders;
 
-ALTER TABLE books
-ADD CONSTRAINT "author_name_unique" UNIQUE(author, name);
+ALTER TABLE users
+ADD COLUMN id serial PRIMARY KEY;
 
-INSERT INTO books(author, name, quantity) VALUES
-('Оноре де Бальзак', 'Гобсек', 200);
+CREATE TABLE orders_to_products(
+    product_id int REFERENCES products(id),
+    order_id int REFERENCES orders(id),
+    quantity int, 
+    PRIMARY KEY(order_id, product_id)
+); 
 
-INSERT INTO books(author, name, quantity) VALUES
-('Оскар Уальд', 'Портрет Доріана Грея', 0);
+-- таблиця1_to_таблиця2
 
-INSERT INTO books(author, name, quantity) VALUES
-('Оноре де Бальза', 'Шагренева Шкіра', 1500);
+
+
+-- ОФОРМЛЕННЯ ЗАМОВЛЕННЯ ДЛЯ ЮЗЕРА
+
+
+-- 1. Створили замовлення
+INSERT INTO orders (customer_id) VALUES
+(4);
+
+-- 2. Наповнити замовлення
+INSERT INTO orders_to_products (product_id, order_id,quantity) VALUES 
+(2, 1, 1),
+(3, 1, 2),
+(4, 1, 1);
