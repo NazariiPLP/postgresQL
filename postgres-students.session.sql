71cf3f512e104fc3f0d330fdc2c2c124d763b77d
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-04-25 20:09:13 +0300

    1:1

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,48 +1,37 @@
 -- Ключ - ознака (найчастіше штучна) яка відрізняє один запис у таблиці від іншого
 -- Первинний ключ (PRIMARY KEY) - використовується для того, щоб ключу дати обмеження унікальності (UNIQUE) і обмеження NOT NULL
--- Група стовпців - стовпець (група стовпців) які могли статти первинним ключем, але ще не обрані як такі
--- Зовнішній ключ (foreing keys) - стовпець (група стовпців) які містять значення, які посилаються на ідентифікатори в інших таблицях 
+-- Потенційний ключ - стовпець (група стовпців) які могли стати первинним ключем, але ще не обрані як такі.
+-- Зовнішній ключ (foreign key) - стовпець (група стовпців) які містять значення, які посилаються на ідентифікатори в інших таблицях
 
 
-/*
-
-Задача: МІНІ-ЮТУБ
-
-Таблиця контенту:
-- назва
-- опис
-- автоп (юзер, який створив контент)
-- дата створення 
+-- 1:1
 
 
-Реакції
-- is_liked:
-    - null - користувач не ставив оцінку взагалі
-    - true - контекнт лайкнули
-    - false - контент дізлайкнули
-
-У контента може бути багато реакцій від користувачів
-Реакції - зв'язок між користувачем і контентом    
-
-
-*/
-
-CREATE TABLE contents(
+CREATE TABLE coaches(
     id serial PRIMARY KEY,
-    name varchar(256) NOT NULL CHECK(name != ''),
-    description text,
-    author_id int REFERENCES users(id),
-    created_at timestamp DEFAULT current_timestamp
+    name varchar(256) NOT NULL CHECK(name != '')
 );
 
-CREATE TABLE reactions(
-    content_id int REFERENCES contents(id),
-    user_id int REFERENCES users(id),
-    is_liked boolean
+CREATE TABLE teams(
+    id serial PRIMARY KEY,
+    name varchar(256),
+    coach_id int REFERENCES coaches(id)
 );
 
-INSERT INTO contents(name, author_id) VALUES
-('Funny dogs', 3);
+-- + 1. Спочатку створюємо тренерів без зовнішнього ключа
+-- + 2. Створюємо таблицю команд (з зовнішнім ключем)
+-- + 3. За допомогою ALTER додати зовнішній ключ до таблиці coaches
 
-INSERT INTO reactions VALUES -- додати реакцію
-(1, 1, true);
+ALTER TABLE coaches
+ADD COLUMN team_id int REFERENCES teams(id);
+
+-------
+
+INSERT INTO coaches(name, team_id) VALUES
+('Ivanov', NULL);
+
+INSERT INTO teams(name, coach_id) VALUES
+('Monreal', 1);
+
+-- Оновлення інформації в таблиці коучів
+-- коуч має відображати, яку команду він тренує (стовпець team_id)
