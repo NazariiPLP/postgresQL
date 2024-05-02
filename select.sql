bb8444909e1182c9373973472fcb04c763e487ec
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-05-02 18:45:45 +0300

    Date operators

--- select.sql
+++ select.sql
@@ -28,3 +28,23 @@ INSERT INTO users (first_name, last_name, email, gender, birthday, is_subscribe)
 
 DELETE FROM users
 WHERE id = 5;
+
+
+---------------------
+
+-- Задача: вивести всіх користувачів з інфою про них + вік
+
+
+SELECT id, first_name, last_name, birthday, extract("years" from age(birthday)) FROM users;
+
+
+UPDATE users
+SET birthday = '2005-01-01'
+WHERE (gender = 'female' AND is_subscribe);
+
+
+----------------------
+-- make_interval(years, month, [days]) - функція, яка створжю власний інтервал
+
+SELECT id, first_name, last_name, make_interval(40, 8) FROM users;
+
