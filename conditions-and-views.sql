8c35b19ddebe7aa89d16365673f205d792b50668
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-05-10 20:26:41 +0300

    Views

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,116 +1,55 @@
-SELECT * FROM users
-WHERE gender is NULL;
 
-ALTER TABLE orders
-ADD COLUMN status boolean;
+-- Вивести інфу про всіх юзерів + інфу скільки хто зробив замовлень
 
-
-
-
-UPDATE orders
-SET status = true
-WHERE id % 2 = 0;
-
-UPDATE orders
-SET status = false
-WHERE id % 2 = 1;
+SELECT u.first_name, u.last_name, u.email, count(o.id) AS "orders_amount" * FROM
+users AS u LEFT JOIN orders AS o
+ON u.id = o.customer_id
+GROUP BY u.id
+ORDER BY "orders_amount";
 
 
-SELECT id, created_at, customer_id, status AS order_status FROM orders;
+CREATE OR REPLACE VIEW users_with_orders_amount AS (
+    SELECT u.first_name, u.last_name, u.email, count(o.id) AS "orders_amount" FROM
+    users AS u LEFT JOIN orders AS o
+    ON u.id = o.customer_id
+    GROUP BY u.id
+    ORDER BY "orders_amount"
+);
+
+SELECT * FROM users_with_orders_amount;
 
 
---- 1 syntax CASE
-
-/*
-CASE 
-    WHEN condition1 = true
-    THEN result1
-    WHEN condition2 = true
-    THEN result2
-    ...
-    ELSE result3
-END;    
-*/
-
--- вивести всі замовлення, там де статус true - написати "виконано", де статус false - написати "нове"
-
-SELECT id, created_at, customer_id (
-    CASE
-    WHEN status = TRUE
-    THEN 'виконано' 
-    WHEN status = FALSE
-    THEN 'нове'
-    ELSE 'інший статус'
-    END
-) AS order_status
-FROM orders
-ORDER BY id;
+-- Отримати email тих юзерів, які мають менше 2 замовлень
 
 
--- 2 syntax CASE
+SELECT email, order_amount FROM users_with_orders_amount
+WHERE orders_amount < 2;
 
-/*
-    CASE condition WHEN value1 THEN result1
-                    WHEN value2 THEN result2
-                    ...
-        ELSE default_result
-    END;
-*/
+-- Можна видаляти вьюхи
 
--- Витягти місяць народження юзера і на його основі вивести, народився восени, навесні, влітку чи взимку
-
-SELECT *, (
-    CASE extract('month' from birthday)
-        WHEN 1 THEN 'winter'
-        WHEN 2 THEN 'winter'
-        WHEN 3 THEN 'spring'
-        WHEN 4 THEN 'spring'
-        WHEN 5 THEN 'spring'
-        WHEN 6 THEN 'summer'
-        WHEN 7 THEN 'summer'
-        WHEN 8 THEN 'summer'
-        WHEN 9 THEN 'fall'
-        WHEN 10 THEN 'fall'
-        WHEN 11 THEN 'fall'
-        WHEN 12 THEN 'winter'
-        ELSE 'unknown'
-    END
-) AS "пора року" FROM users;
+DROP VIEW with orders_amount;
 
 
 
-/*
+-- Створити предствлення, яке зберігає замовлення з їхньою вартістю
+-- Додамо до цієї вьюхи інформацію про замовника (id замовника)
 
-Задача 1
+CREATE OR REPLACE VIEW orders_with_price AS (
+    SELECT o.id, o.customer_id, sum(p.price * otp.quantity) AS "order_sum", o.customer_id, o.status FROM
+    orders AS o JOIN orders_to_products AS otp
+    ON o.id = otp.order_id
+    JOIN products AS p
+    ON p.id = otp.products_id
+    GROUP BY o.id
+);
 
-Вивести юзерів, в яких в стовпці "стать_прописом" буде українською прописано "чоловік", "жінка", "інший варіант"
-
-Задача 2
-
-Вивести всі товари з  таблиці products
-Якщо ціна більше 6 тис - флагман
-Якщо ціна від до 2 до 6 тисяч - середній клас
-Якщо ціна менше 2 тис - бюджетний
-
-*/
-
-
-SELECT id, name (
-    CASE gender
-        WHEN "male" THEN "чоловік"
-        WHEN "female" THEN "жінка"
-        ELSE "інший варіант"
-END
-) AS "стать прописом" FROM users;
+DROP VIEW orders_with_price;
 
 
-SELECT *, (
-    CASE price
-        WHEN price > 6000 THEN "флагман"
-        WHEN price BETWEEN 2000 AND 6000 THEN "середній клас"
-        WHEM price < 2000 THEN "бюджетний"
-        ELSE 'Інша невідома цінова категорія'
-    END
-) AS product_rate
-FROM products
-ORDER BY price;
+-- Вивести юзерів з сумою коштів, яку вони витратили у нашому магазині
+
+SELECT u.id. u.email, sum(owp.order_sum) AS "sum" FROM 
+users AS u LEFT JOIN orders_with_price AS owp
+ON u.id = owp.customer_id
+GROUP BY u.id
+ORDER BY sum;
